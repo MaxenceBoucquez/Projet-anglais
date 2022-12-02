4cbfb990f03afe9012b09ca235e3585f765a5107
Derfire22 <derfire22@gmail.com>
2022-12-02 17:08:27 +0100

    clean code

--- projet_anglais_test.py
+++ projet_anglais_test.py
@@ -14,25 +14,31 @@ drawCanv = Canvas(
 screenWidth = drawCanv.winfo_screenwidth()
 screenHeight = drawCanv.winfo_screenheight()
 
+# keys
+
+# define the width of each key
+# We have 8 white keys
+whiteKeyWidth = screenWidth / 8
+blackKeyWidth = screenWidth / 48
+
 
 # Definition of the event : for each key, when we click on it, it plays the appropriated sound
 def clicked(event):
-    
-    if(event.x < ( screenWidth/ 8)):
+    if(event.x < ()):
         play("do1")
-    elif(event.x < ((screenWidth / 8) * 2)):
+    elif(event.x < (whiteKeyWidth * 2)):
         play("re")
-    elif(event.x < ((screenWidth / 8) * 3)):
+    elif(event.x < (whiteKeyWidth * 3)):
         play("mi")
-    elif(event.x < ((screenWidth / 8) * 4)):
+    elif(event.x < (whiteKeyWidth * 4)):
         play("fa")
-    elif(event.x < ((screenWidth / 8) * 5)):
+    elif(event.x < (whiteKeyWidth * 5)):
         play("sol")
-    elif(event.x < ((screenWidth / 8) * 6)):
+    elif(event.x < (whiteKeyWidth * 6)):
         play("la")
-    elif(event.x < ((screenWidth / 8) * 7)):
+    elif(event.x < (whiteKeyWidth * 7)):
         play("si")
-    elif(event.x < ((screenWidth / 8) * 8)):
+    elif(event.x < (whiteKeyWidth * 8)):
         play("do2")
 
 
@@ -41,16 +47,16 @@ drawCanv.bind('<Button>', clicked)
 # Drawing of the white keys of the piano
 for i in range(0,9):
     rectangle = drawCanv.create_rectangle(
-        (screenWidth / 8) * i, 0,
-        (screenWidth / 8) * i+1, screenHeight,\
+        whiteKeyWidth * i, 0,
+        whiteKeyWidth * i + 1, screenHeight,\
         outline = 'black')
 
 # Drawing of the black keys of the piano
 for i in range(0,6):
     if(i!=3):
         rectangle = drawCanv.create_rectangle(
-            (screenWidth/8) * i+5 * (screenWidth/48),0,
-            (screenWidth/8) * (i+1) + 5 + (screenWidth/48),screenHeight/(8/5),\
+            whiteKeyWidth * i+5 * blackKeyWidth,0,
+            whiteKeyWidth * (i+1) + 5 + blackKeyWidth,screenHeight / (8/5),\
             outline='black', fill='black')
             
 # Map the click event to trigger sounds
