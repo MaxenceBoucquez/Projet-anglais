0ff529f850c869ea94fd72a78ad4659303e1f200
nameko38 <94040730+nameko38@users.noreply.github.com>
2022-12-09 17:19:52 +0100

    add buttons

--- sounds.py
+++ sounds.py
@@ -1,10 +1,14 @@
 import simpleaudio as sa
 
+listener = None
+
 def play(note):
     wave_obj = sa.WaveObject.from_wave_file(f"Music_notes/{note}.wav")
     wave_obj.play()
+    if listener:
+        listener(note)
 
 def playback(data):
     return
 
-def record(n):
+def record():
