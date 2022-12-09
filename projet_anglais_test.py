e0c6e26e12eb606f32f06a770afdc6f6b0d294fa
nameko38 <94040730+nameko38@users.noreply.github.com>
2022-12-09 16:50:43 +0100

    functionnal black notes Co-authored-by: Cybiot <Cybiot@users.noreply.github.com> Co-authored-by: sakured <sakured@users.noreply.github.com> Co-authored-by: Elliot <Derfire22@users.noreply.github.com>

--- projet_anglais_test.py
+++ projet_anglais_test.py
@@ -19,11 +19,15 @@ screenHeight = drawCanv.winfo_screenheight()
 # define the width of each key
 # We have 8 white keys
 whiteKeyWidth = screenWidth / 8
-blackKeyWidth = screenWidth / 48
+blackKeyWidth = screenWidth / 24
 
 
 # Definition of the event : for each key, when we click on it, it plays the appropriated sound
 def clicked(event):
+    if not (blackkey_click(event)):
+        whitekey_click(event)
+
+def whitekey_click(event):
     if(event.x < whiteKeyWidth):
         play("do1")
     elif(event.x < whiteKeyWidth * 2):
@@ -40,8 +44,26 @@ def clicked(event):
         play("si")
     elif(event.x < whiteKeyWidth * 8):
         play("do2")
-
-
+        
+def blackkey_click(event):
+    if (event.y < screenHeight / (8/5)):
+        if (event.x > whiteKeyWidth - blackKeyWidth/2 and event.x < whiteKeyWidth + blackKeyWidth/2):
+            play("doSharp")
+            return True
+        if (event.x > whiteKeyWidth * 2 - blackKeyWidth/2 and event.x < whiteKeyWidth * 2 + blackKeyWidth/2):
+            play("reSharp")
+            return True
+        if (event.x > whiteKeyWidth * 4 - blackKeyWidth/2 and event.x < whiteKeyWidth * 4 + blackKeyWidth/2):
+            play("faSharp")
+            return True
+        if (event.x > whiteKeyWidth * 5 - blackKeyWidth/2 and event.x < whiteKeyWidth * 5 + blackKeyWidth/2):
+            play("solSharp")
+            return True
+        if (event.x > whiteKeyWidth * 6- blackKeyWidth/2 and event.x < whiteKeyWidth * 6 + blackKeyWidth/2):
+            play("siBemol")
+            return True
+    return False
+    
 
 drawCanv.bind('<Button>', clicked) 
 # Drawing of the white keys of the piano
@@ -52,11 +74,11 @@ for i in range(0,9):
         outline = 'black')
 
 # Drawing of the black keys of the piano
-for i in range(0,6):
+for i in range(1,7):
     if(i!=3):
         rectangle = drawCanv.create_rectangle(
-            whiteKeyWidth * i+5 * blackKeyWidth,0,
-            whiteKeyWidth * (i+1) + 5 + blackKeyWidth,screenHeight / (8/5),\
+            whiteKeyWidth * i - blackKeyWidth / 2,0,
+            whiteKeyWidth * i + blackKeyWidth / 2,screenHeight / (8/5),\
             outline='black', fill='black')
             
 # Map the click event to trigger sounds
